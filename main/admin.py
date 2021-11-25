babdad00ea63ac7a5c924cd48962d1788331c51c
Felipe Martins <felipe.souzagsm@gmail.com>
2021-11-25 14:00:41 -0300

    chore: configurando a aparição do gerar pdf

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from .models import Category, News, Comment
 
+@admin.action(description='Gerar PDF selecionados')
+def gerar_pdf(modeladmin, request, queryset):
+    pass
+
 admin.site.register(Category)
 
 class AdminNews(admin.ModelAdmin):
     list_display = ('title', 'category', 'add_time')
+    actions = [gerar_pdf]
 
 admin.site.register(News,AdminNews)
 
